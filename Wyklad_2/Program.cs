8fd4d69940a609f1f5d8336ca27bbee635b4716f
Stanislaw Kubik <s19264@pjwstk.edu.pl>
2021-03-17 19:18:20 +0100

    test

---
+++ Wyklad_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wyklad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
